ef8103e48bb7fc9d9d69dbbefba56c023429e5c0
ArsiaArdalan <marsian@protonmail.com>
2016-11-12 22:00:49 -0500

    branch1: fixes alignement of the ListView to be anchored to the top of Send button, with a reasonable bottom margin of 10dp.

--- app/src/main/java/com/example/AndroidDevelopmentClass/Catalog/ChatWindow.java
+++ app/src/main/java/com/example/AndroidDevelopmentClass/Catalog/ChatWindow.java
@@ -2,6 +2,9 @@ package com.example.AndroidDevelopmentClass.Catalog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class ChatWindow extends AppCompatActivity {
 
@@ -10,4 +13,9 @@ public class ChatWindow extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
     }
+
+    ListView listView = (ListView) findViewById(R.id.listView);
+    Button button = (Button) findViewById(R.id.sendButton);
+    EditText editText = (EditText) findViewById(R.id.editText);
+
 }
